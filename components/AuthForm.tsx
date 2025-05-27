ab1555a7286aeaa1cc6ec6fe8752d478fc098c5b
ddv2311 <darshitdvyas@gmail.com>
2025-05-28 03:21:59 +0530

    Auth and storage implemented

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,21 +4,21 @@ import { z } from "zod";
 import Link from "next/link";
 import Image from "next/image";
 import { toast } from "sonner";
-// import { auth } from "@/firebase/client";
+import { auth } from "@/firebase/client";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-// import {
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-// } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
-// import { signIn, signUp } from "@/lib/actions/auth.action";
-import FormField from "@/components/FormField";
+import { signIn, signUp } from "@/lib/actions/auth.action";
+import FormField from "./FormField";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -102,7 +102,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100">InterviewPro</h2>
+          <h2 className="text-primary-100">PrepWise</h2>
         </div>
 
         <h3>Practice job interviews with AI</h3>
